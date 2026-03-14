60a0bf186423b3ebec3a68176e9ff8842892ec29
james-sheridan-code <jamessheridan007@gmail.com>
2026-03-14 08:09:16 +0200

    Refactor Binary Logistic Regression implementation
    Updated the implementation of Binary Logistic Regression to improve clarity and functionality. Added return values for weights and bias in the mini_batch_gradient_descent function.

--- BLR_BCE_MBLR.py
+++ BLR_BCE_MBLR.py
@@ -1,10 +1,9 @@
 """
-Manually Coding a Binary Logistic Regression.
+Manual implementation of a Binary Linear Regression using NumPy.
 
-Model Architecture: Y = sigmoid(W @ X + b)
-Cost Function: BCE (Binary Cross-Entropy)
-Optimisation: MBGD (Mini-Batch Gradient Descent)
-
+Model:          Y = sigmoid(W @ X + b)
+Cost Function:  Binary Cross-Entropy (BCE)
+Optimisation:   Mini-Batch Gradient Descent (MBGD)
 """
 
 import numpy as np
@@ -15,18 +14,35 @@ def main():
 
     X_train = standardise(X_train)
 
-    mini_batch_gradient_descent(W=np.array([0.0,0.0]), b=0, learn_rate=0.01, 
+    W, b = mini_batch_gradient_descent(W=np.array([0.0,0.0]), b=0, learn_rate=0.01, 
                                 iterations=10000, y_train=y_train, X_train=X_train, batch_size = 1)
+    print(f"Final Results:\nw: {W.flatten()}\nb: {b:.2f}")
 
 
 def standardise(variable):
+    """
+    Standardise data to have: mean = 0, and standard deviation = 1.
+    Used to make gradient descent converge faster and more reliably.
+    
+    Returns the X matrix with each column standardised. 
+    """
     mean = np.mean(variable, axis=0)
     std = np.std(variable, axis=0)
     return (variable - mean) / std
 
 
 def mini_batch_gradient_descent(W, b, learn_rate, iterations, y_train, X_train, batch_size):
-    m = len(y_train)
+    """
+    Train a binary logistic regression model using Mini-Batch Gradient Descent.
+
+    The training data is shuffled at the start of each iteration and
+    divided into smaller batches. For each batch, the gradient of the
+    Binary Cross-Entropy (BCE) cost function is computed and the model
+    parameters (weights and bias) are updated.
+
+    Returns the final weight matrix (W) and the final bias (b).
+    """
+    m = X_train.shape[0]
     W = W.reshape(-1,1)
     y_train = y_train.reshape(-1,1)
 
@@ -50,17 +66,13 @@ def mini_batch_gradient_descent(W, b, learn_rate, iterations, y_train, X_train, 
             db = (1/batch_size) * np.sum(y_pred - y_batch)
 
             # update gradients
-            temp_W = W - learn_rate * dw
-            temp_b = b - learn_rate * db
-            W = temp_W
-            b = temp_b
+            W -= learn_rate * dw
+            b -= learn_rate * db
 
             if i%100 == 0:
-                print(f"Iteration: {i} Batch {j}\tCost: {cost:.4f}\tW: {W[0,0]:.2f}, {W[1,0]:.2f}\tb: {b:.2f}")
-
-    print(f"Final Results:\nw: {W[0,0]:.2f}, {W[1,0]:.2f}\nb: {b:.2f}")
+                print(f"Iteration: {i} Batch {j}\tCost: {cost:.4f}\tW: {W.flatten()}\tb: {b:.2f}")
+    return W, b
 
 
 if __name__ == '__main__':
-
     main()
